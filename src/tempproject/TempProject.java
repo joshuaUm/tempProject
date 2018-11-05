58d42ca6f9b08b15a1e2b0492666f7e798c4d58d
JoshuaLee Um-146004429 <146004429@vusdnoc.vusd.k12.ca.us>
2018-11-05 12:42:22 -0800

    Initial Commit

---
+++ src/tempproject/TempProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempproject;
+
+/**
+ *
+ * @author 146004429
+ */
+public class TempProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("gitHub works");
+    }
+    
+}
